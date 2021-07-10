3f0969338e953537f3ec5140b1d8c8c2befcb53c
helgakern <helgakern@gmail.com>
2021-07-09 17:08:45 -0700

    add header and styling

--- App.js
+++ App.js
@@ -5,7 +5,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.h1}>Pokédex</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +16,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f0f0', 
+    alignItems: 'flex-start', 
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  header: {
+    flex: 1,
+    backgroundColor: '#ee1515',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 20,
+    height: '20%',
+    borderBottomColor: '#222224',
+    borderBottomWidth: 5
   },
+  h1: {
+    fontSize: 40,
+    color: '#FFCB05',
+    textShadowColor: 'black',
+    textShadowRadius: 10,
+    shadowOffset: {width: 2, height: 2},
+    shadowOpacity: 1,
+  }
 });
