f85d18372bdec7beb14f995728e112aa29bf4af8
helgakern <helgakern@gmail.com>
2021-07-10 20:49:32 -0700

    Add images and style

--- components/PokemonDetail/index.js
+++ components/PokemonDetail/index.js
@@ -1,10 +1,51 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
-export default function PokemonDetail() {
+function PokemonDetail(props) {
+  const { route } = props;
+  const { params } = route;
+
+  const [pokemon, setPokemon] = useState({});
+
+  useEffect(() => {
+    fetch(params.pokemon.url)
+      .then(res => res.json())
+      .then(data => {
+        setPokemon(data)
+      })
+  }, [])
+
   return(
-    <View>
-      <Text>Hello</Text>
+    <View style={styles.container}>
+      { pokemon.name ? (
+        <>
+          <Image style={styles.image} source={{ uri: pokemon.sprites.front_default}}/>
+          <Text style={styles.h1}>{pokemon.name}</Text>
+          <Text style={styles.text}>Weight: {pokemon.weight}</Text>
+        </>
+        ) :
+        <Text>Loading...</Text>
+      }
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  image: {
+    height: 300,
+    width: 300
+  },
+  h1: {
+    fontSize: 50
+  },
+  text: {
+    fontSize: 25
+  }
+})
+
+export default PokemonDetail
+// {"mode":"full","isActive":false}
